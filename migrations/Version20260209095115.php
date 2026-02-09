aeb67610e1c2d659f170a3ef4c5469320665787b
Boniben <benjamin.boniface@sts-sio-caen.info>
2026-02-09 10:52:21 +0100

    reprise du projet apres bug

--- migrations/Version20260209095115.php
+++ migrations/Version20260209095115.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260209083911 extends AbstractMigration
+final class Version20260209095115 extends AbstractMigration
 {
     public function getDescription(): string
     {
