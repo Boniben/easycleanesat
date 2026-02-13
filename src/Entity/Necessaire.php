26c6aa554e84ae7f6622bb36113b04ffdd2a6cf2
unknown <admin@B23-09.info.local>
2026-02-13 11:27:45 +0100

    Remodelisation Actions et Necessaire

--- src/Entity/Necessaire.php
+++ src/Entity/Necessaire.php
@@ -2,29 +2,32 @@
 
 namespace App\Entity;
 
-use App\Repository\SupportRepository;
+use App\Repository\NecessaireRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: SupportRepository::class)]
-class Support
+#[ORM\Entity(repositoryClass: NecessaireRepository::class)]
+class Necessaire
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 200)]
     private ?string $nom = null;
 
-    #[ORM\Column(length: 10)]
+    #[ORM\Column(length: 50)]
     private ?string $code = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $type = null;
 
     /**
      * @var Collection<int, Actions>
      */
-    #[ORM\OneToMany(targetEntity: Actions::class, mappedBy: 'support')]
+    #[ORM\ManyToMany(targetEntity: Actions::class, mappedBy: 'necessaire')]
     private Collection $actions;
 
     public function __construct()
@@ -61,6 +64,18 @@ class Support
         return $this;
     }
 
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
     /**
      * @return Collection<int, Actions>
      */
@@ -73,7 +88,7 @@ class Support
     {
         if (!$this->actions->contains($action)) {
             $this->actions->add($action);
-            $action->setSupport($this);
+            $action->addNecessaire($this);
         }
 
         return $this;
@@ -82,10 +97,7 @@ class Support
     public function removeAction(Actions $action): static
     {
         if ($this->actions->removeElement($action)) {
-            // set the owning side to null (unless already changed)
-            if ($action->getSupport() === $this) {
-                $action->setSupport(null);
-            }
+            $action->removeNecessaire($this);
         }
 
         return $this;
