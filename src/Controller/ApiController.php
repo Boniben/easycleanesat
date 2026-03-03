8bc32001b30e83b187f79f3e20843f448472a387
EstelleBihel <estelle.bihel@sts-sio-caen.info>
2026-03-03 14:50:04 +0100

    US-C8 : TypeSupport multi-sélection blocs, API search TypeSupport, description TypeSupport, fix ZonesClient entity

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\TypeSupportRepository;
 
 #[Route('/api')]
 class ApiController extends AbstractController
@@ -77,5 +78,18 @@ class ApiController extends AbstractController
         }, $zones);
 
         return $this->json($data);
+    }
+    #[Route('/type-support/search', name: 'api_type_support_search', methods: ['GET'])]
+    public function searchTypeSupport(Request $request, TypeSupportRepository $typeSupportRepository): JsonResponse
+    {
+        $query = trim($request->query->get('q', ''));
+
+        if ($query === '') {
+            $results = [];
+        } else {
+            $results = $typeSupportRepository->searchByNomOrDescription($query);
+        }
+
+        return $this->json($results);
     }
 }
